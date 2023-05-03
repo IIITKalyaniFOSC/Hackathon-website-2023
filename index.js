d49ab93d82eddc7ba70e3c8f45cd78490a5c838c
Rishi Kumar <rsi.dev17@gmail.com>
2023-05-03 11:35:18 +0530

    Postpone date to August

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
     mm = String(today.getMonth() + 1).padStart(2, "0"),
     yyyy = today.getFullYear(),
     nextYear = yyyy,
-    dayMonth = "05/06/",
+    dayMonth = "08/04/",
     birthday = dayMonth + yyyy;
 
   today = mm + "/" + dd + "/" + yyyy;
