797e38abb18fc1c0c5bac3490954ceb6b916e2c3
sagamantus <74814583+sagamantus@users.noreply.github.com>
2023-06-27 00:45:26 +0530

    Fix reload loop for http

--- main.js
+++ main.js
@@ -6,6 +6,14 @@ initTE({ Tab });
 
 // Redirect to https
 if (window.location.protocol === 'http:') {
-    window.location.href = 'https://' + window.location.host + window.location.pathname;
-    window.location.reload(true);
+    window.location.replace('https://' + window.location.host + window.location.pathname);
 }
+
+window.addEventListener('load', function() {
+  var script = document.createElement('script');
+  script.src = 'https://apply.devfolio.co/v2/sdk.js';
+  script.async = true;
+  script.defer = true;
+  // Append the script to the <body> section
+  document.body.appendChild(script);
+});
