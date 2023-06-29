be0b91ead425d6470d7a06062cf35b818c2904cd
Ayushi <ayushi0628@gmail.com>
2023-06-29 22:12:11 +0530

    fix: sponsors-section

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ initTE({ Collapse });
 initTE({ Tab });
 
 // Redirect to https
-if (window.location.protocol === 'http:') {
-    window.location.replace('https://' + window.location.host + window.location.pathname);
-}
+// if (window.location.protocol === 'http:') {
+//     window.location.replace('https://' + window.location.host + window.location.pathname);
+// }
 
-window.addEventListener('load', function() {
-  var script = document.createElement('script');
-  script.src = 'https://apply.devfolio.co/v2/sdk.js';
-  script.async = true;
-  script.defer = true;
-  // Append the script to the <body> section
-  document.body.appendChild(script);
-});
+// window.addEventListener('load', function() {
+//   var script = document.createElement('script');
+//   script.src = 'https://apply.devfolio.co/v2/sdk.js';
+//   script.async = true;
+//   script.defer = true;
+//   // Append the script to the <body> section
+//   document.body.appendChild(script);
+// });
